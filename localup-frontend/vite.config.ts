6ef12da4b88d8e8bd645f7a94c2d1453fbb56bff
Benmammar Rayan <rayan.benmammar90@gmail.com>
2025-04-12 17:25:25 +0200

    [FE]: set up eslint and prettier

--- localup-frontend/vite.config.ts
+++ localup-frontend/vite.config.ts
@@ -1,14 +1,23 @@
-import path from "path"
-import tailwindcss from "@tailwindcss/vite"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    eslintPlugin({
+      include: ['src/**/*.ts', 'src/**/*.tsx'],
+    }),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
