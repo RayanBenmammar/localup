8f1c57c61afd758ec81e6083517b4cfea163f052
Benmammar Rayan <rayan.benmammar90@gmail.com>
2025-04-13 22:06:29 +0200

    [FE]: update style on login and register forms

--- localup-frontend/src/pages/login.tsx
+++ localup-frontend/src/pages/login.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from '@/features/auth/components/LoginForm.tsx';
 
 export function LoginPage() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-svh md:min-w-lg w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <LoginForm />
       </div>
--- localup-frontend/src/pages/register.tsx
+++ localup-frontend/src/pages/register.tsx
@@ -2,7 +2,7 @@ import { RegisterForm } from '@/features/auth/components/RegisterForm';
 
 export function RegisterPage() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <div className="flex min-h-svh md:min-w-lg w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <RegisterForm />
       </div>
